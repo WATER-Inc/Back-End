4559e87a893f0411cf1f49ffde68e3e8a2d4be22
Falkramm <matus.daniil@mail.ru>
2023-05-22 21:58:01 +0300

    Add Action Manager

---
+++ src/main/java/action/ActionManager.java
@@ -0,0 +1,20 @@
+package action;
+
+import dao.PersistException;
+import service.ServiceFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ActionManager {
+    private ServiceFactory factory;
+
+    public ActionManager(ServiceFactory factory) {
+        this.factory = factory;
+    }
+
+    public Action.Forward execute(Action action, HttpServletRequest request, HttpServletResponse response) throws PersistException {
+        action.setFactory(factory);
+        return action.exec(request, response);
+    }
+}
