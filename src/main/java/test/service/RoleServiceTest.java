d565a00fbd3310d3c0790e53125241a641105259
KirillStepankov-bsu <fpm.stepanko@bsu.by>
2023-05-28 14:55:42 +0300

    RoleServiceTests

---
+++ src/main/java/test/service/RoleServiceTest.java
@@ -0,0 +1,72 @@
+package test.service;
+
+
+import dao.PersistException;
+import entity.Role;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+@Test
+public class RoleServiceTest extends ServiceTest<Role> {
+
+    @BeforeTest
+    @Override
+    public void init() throws PersistException {
+        super.init();
+        service = factory.getService(Role.class);
+    }
+
+    @Override
+    public void getByIdTest() throws PersistException {
+        Role role = new Role("test");
+        role = (Role) service.persist(role);
+        Role retrievedRole = (Role) service.getById(role.getId());
+        service.delete(role);
+        assertEquals(role, retrievedRole);
+    }
+
+    @Override
+    public void getAllTest() throws PersistException {
+        Role role1 = new Role("test1");
+        role1 = (Role) service.persist(role1);
+        Role role2 = new Role("test2");
+        role2 = (Role) service.persist(role2);
+        List<Role> roleList = (List<Role>) service.getAll();
+        service.delete(role1);
+        service.delete(role2);
+        assertEquals(2, roleList.size());
+        assertTrue(roleList.contains(role1));
+        assertTrue(roleList.contains(role2));
+    }
+
+    @Override
+    public void persistTest() throws PersistException {
+        Role role = new Role("test");
+        Role persistedRole = (Role) service.persist(role);
+        assertNotNull(persistedRole.getId());
+        Role retrievedRole = (Role) service.getById(persistedRole.getId());
+        service.delete(persistedRole);
+        assertEquals(persistedRole, retrievedRole);
+    }
+
+    @Override
+    public void deleteTest() throws PersistException {
+        Role role = new Role("test");
+        role = (Role) service.persist(role);
+        service.delete(role);
+        assertNull(service.getById(role.getId()));
+    }
+
+    @Override
+    public void updateTest() throws PersistException {
+        Role role = new Role("test");
+        role = (Role) service.persist(role);
+        role.setTitle("updated");
+        service.update(role);
+        Role retrievedRole = (Role) service.getById(role.getId());
+        service.delete(role);
+        assertEquals(role, retrievedRole);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kirill
+  Date: 28.05.23
+  Time: 14:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>error</title>
+</head>
+<body>
+<h1>ERROR</h1> something is wrong ))))))) check out log output
+</body>
+</html>
