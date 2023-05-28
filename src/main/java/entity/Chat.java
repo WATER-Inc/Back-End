87e51c012c8193754330e52468dd22623c793a1a
Kirill Stepankov <kirillstepankov18@gmail.com>
2023-05-28 19:30:02 +0300

    ChatServiceTest

--- src/main/java/entity/Chat.java
+++ src/main/java/entity/Chat.java
@@ -1,12 +1,21 @@
 package entity;
 
-import dao.Identified;
 import dao.entity.Participants;
+
+import java.util.Objects;
 
 public class Chat extends Entity {
     private String id;
     private String name;
     private Participants participants;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chat chat = (Chat) o;
+        return Objects.equals(id, chat.id) && Objects.equals(name, chat.name);
+    }
 
     public String getName() {
         return name;
--- src/main/java/service/ChatService.java
+++ src/main/java/service/ChatService.java
@@ -1,9 +1,13 @@
 package service;
 
 import dao.PersistException;
+import dao.entity.ChatLink;
+import dao.entity.Participants;
 import dao.mysql.MySqlChatDAO;
+import dao.mysql.MySqlChatLinkDAO;
 import entity.Chat;
 import entity.Entity;
+import entity.Role;
 import entity.User;
 
 import java.util.List;
@@ -29,6 +33,36 @@ public class ChatService extends Service {
 
     @Override
     public Chat persist(Entity object) throws PersistException {
-        return (Chat) super.persist(object);
+        Chat chat = (Chat) super.persist(object);
+        chat.setParticipants(((Chat) object).getParticipants());
+        addUsers((Chat) chat);
+        return getById(chat.getId());
+    }
+
+    public void addUser(Chat chat, User user, Role role) throws PersistException {
+        ChatLink chatLink = new ChatLink();
+        chatLink.setUserId(user.getId());
+        chatLink.setRoleId(role.getId());
+        chatLink.setChatId(chat.getId());
+        daoFactory.getDao(daoFactory.getConnection(), ChatLink.class).persist(chatLink);
+    }
+
+    @Override
+    public void delete(Entity object) throws PersistException {
+        super.delete(object);
+    }
+
+    public void addUsers(Chat chat) throws PersistException {
+        MySqlChatLinkDAO mySqlChatLinkDAO = (MySqlChatLinkDAO) daoFactory.getDao(daoFactory.getConnection(), ChatLink.class);
+        Participants participants = chat.getParticipants();
+        List<User> users = participants.getUsers();
+        List<Role> roles = participants.getRoles();
+        ChatLink link = new ChatLink();
+        for (int i = 0; i < users.size(); ++i) {
+            link.setChatId(chat.getId());
+            link.setUserId(users.get(i).getId());
+            link.setRoleId(roles.get(i).getId());
+            mySqlChatLinkDAO.persist(link);
+        }
     }
 }
